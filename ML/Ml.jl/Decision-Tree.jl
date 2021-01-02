1324c381dae163c080fddca7b4bbbc086644f990
ylfeng <ylfeng@ir.hit.edu.cn>
2021-01-02 17:31:18 +0800

    加入 Python 版本的 ID3 决策树算法实现

--- ML/Ml.jl/Decision-Tree.jl
+++ ML/Ml.jl/Decision-Tree.jl
@@ -2,7 +2,7 @@ using DataFrames, CSV;
 using Serialization;
 
 function calcEnt(datasets)
-    num_samples = size(datasets,1)
+    num_samples = size(datasets, 1)
     label_counts = Dict()
     for label in datasets[end]
         if label in keys(label_counts)
@@ -15,37 +15,37 @@ function calcEnt(datasets)
     return - sum([p * log2(p) for p in values(label_counts) ./ num_samples ])
 end
 
-function calcGain(datasets,class,ent = calcEnt(datasets))
-    num_samples = size(datasets,1)
+function calcGain(datasets, class, ent=calcEnt(datasets))
+    num_samples = size(datasets, 1)
     groups = groupby(datasets, class)
-    entries = [(size(subsets,1),calcEnt(subsets)) for subsets in groups]
+    entries = [(size(subsets, 1), calcEnt(subsets)) for subsets in groups]
     # $Gain(D,a) = Ent(D) - ∑_{v ∈ V} |D^v| / |D| Ent(D^v)$
-    return ent - sum([group_size / num_samples * grout_ent for (group_size,grout_ent) in entries])
+    return ent - sum([group_size / num_samples * grout_ent for (group_size, grout_ent) in entries])
 end
 
-function calcGainRatio(datasets,class,ent = calcEnt(datasets))
-    num_samples = size(datasets,1)
+function calcGainRatio(datasets, class, ent=calcEnt(datasets))
+    num_samples = size(datasets, 1)
     groups = groupby(datasets, class)
-    entries = [(size(subsets,1),calcEnt(subsets)) for subsets in groups]
-    gain =  ent - sum([group_size / num_samples * grout_ent for (group_size,grout_ent) in entries])
+    entries = [(size(subsets, 1), calcEnt(subsets)) for subsets in groups]
+    gain =  ent - sum([group_size / num_samples * grout_ent for (group_size, grout_ent) in entries])
     # $IV = - ∑^V_{v=1} |D^v| / |D| log2(|D^v| / |D|)$
-    IV = - sum([group_size / num_samples * log2(group_size / num_samples) for (group_size,grout_ent) in entries])
+    IV = - sum([group_size / num_samples * log2(group_size / num_samples) for (group_size, grout_ent) in entries])
     # $Gain(D,a) = Gain(D,a) / IV$
     return gain / IV
 end
 
 function maxLabel(datasets)
-    return by(datasets, :好瓜, df -> DataFrame(N = size(df, 1)))
+    return by(datasets, :好瓜, df -> DataFrame(N=size(df, 1)))
 end
 
-function createTree(datasets, classes = names(datasets))
-    groups = groupby(datasets,classes[end])
+function createTree(datasets, classes=names(datasets))
+    groups = groupby(datasets, classes[end])
     if length(groups) == 1          # 取出分类标签（好瓜/坏瓜），如果只有一种，不需要继续分割
         return groups[end][end][end]
     end
     if length(classes) == 1         # 没有别的类别可以用来分割，取概率最大的分类标签
         max_group = groups[1]
-        for group in groups[2:end-1]
+        for group in groups[2:end - 1]
             if length(group) > length(max_group)
                 max_group = group
             end
@@ -54,9 +54,9 @@ function createTree(datasets, classes = names(datasets))
     end
 
     best_class = classes[1]         # 取出最好的分类类别，即信息增益最大的类别
-    best_gain = calcGain(datasets,best_class)
-    for class in classes[1:end-1]
-        curr_gain = calcGain(datasets,class)
+    best_gain = calcGain(datasets, best_class)
+    for class in classes[1:end - 1]
+        curr_gain = calcGain(datasets, class)
         if curr_gain > best_gain
             best_class = class
             best_gain = curr_gain
@@ -64,30 +64,30 @@ function createTree(datasets, classes = names(datasets))
     end
 
     new_classes = copy(classes)     # 递归调用，下一个分支需要用到的分类标签
-    filter!(x->x!=best_class, new_classes)   # 由于julia传引用，需要拷贝一份
+    filter!(x -> x != best_class, new_classes)   # 由于julia传引用，需要拷贝一份
     best_class_label = Set(datasets[best_class])
     tree = Dict(best_class => Dict())
 
     # 遍历该类别标准下所有子分类，递归求解
-    for group in groupby(datasets,best_class)
-        tree[best_class][group[best_class][1]] = createTree(group,new_classes)
+    for group in groupby(datasets, best_class)
+        tree[best_class][group[best_class][1]] = createTree(group, new_classes)
     end
 
     return tree
 end
 
-datasets = CSV.read("Work/ML/2.0.csv");
+datasets = CSV.read("data/2.0.csv", DataFrame);
 tree = createTree(datasets)
 
 print(tree)
 
 describe(datasets)
-by(datasets, :好瓜, df -> DataFrame(N = size(df, 1)))
+by(datasets, :好瓜, df -> DataFrame(N=size(df, 1)))
 
-stream = open("Work/ML/tree.data","w")
+stream = open("data/tree.data", "w")
 serialize(stream,tree)
 close(stream)
 
-stream = open("Work/ML/tree.data","r")
+stream = open("data/tree.data", "r")
 read_tree = deserialize(stream)
 close(stream)
---
+++ ML/Ml.py/decision_tree.py
@@ -0,0 +1,58 @@
+from math import log2
+import json
+
+import pandas
+from pandas import DataFrame
+
+
+def calcEnt(datasets: DataFrame):
+    num_samples = len(datasets)
+    label_counts = datasets.groupby(datasets.columns[-1]).size()
+    # $ Ent(D) = - ∑_{p ∈ P} p log_2(p) $
+    return - sum([p * log2(p) / num_samples for p in label_counts])
+
+
+def calcGain(datasets: DataFrame, class_, ent=None):
+    if ent is None:
+        ent = calcEnt(datasets)
+    num_samples = len(datasets)
+    groups = datasets.groupby(class_).apply(lambda x: (len(x), calcEnt(x)))
+
+    # $Gain(D,a) = Ent(D) - ∑_{v ∈ V} |D^v| / |D| Ent(D^v)$
+    return ent - sum([group_size / num_samples * group_ent for group_size, group_ent in groups])
+
+
+def createTree(datasets: DataFrame, classes=None):
+    if classes is None:
+        classes = datasets.columns[:-1]
+
+    groups = datasets.groupby('好瓜')
+
+    if len(groups) == 1:  # 取出分类标签（好瓜/坏瓜），如果只有一种，不需要继续分割
+        keys = list(groups.groups.keys())
+        return keys[0]
+
+    if len(classes) == 1:  # 没有别的类别可以用来分割，取概率最大的分类标签
+        return groups.size().idxmax()
+
+    class_gains = {class_: calcGain(datasets, class_) for class_ in classes}
+    best_class, best_gains = max(class_gains.items(), key=lambda x: x[1])
+
+    tree = {best_class: {}}
+    search_classes = [key for key in classes if key != best_class]
+    for sub_type, group in datasets.groupby(best_class):
+        sub_tree = createTree(group, search_classes)
+        tree[best_class][sub_type] = sub_tree
+
+    return tree
+
+
+def main():
+    dataset = pandas.read_csv('data/2.0.csv')
+
+    tree = createTree(dataset)
+    print(json.dumps(tree, ensure_ascii=False, indent=2))
+
+
+if __name__ == '__main__':
+    main()
